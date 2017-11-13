a121841d8e38315b4bfbb9ddb9ea4fafc8566f2f
Bepen Neupane <bepen3@gmail.com>
2017-11-13 17:15:20 -0500

    push

--- CSG.c
+++ CSG.c
@@ -17,7 +17,7 @@ struct CSG* newCSG(char* course, int studentID, char* grade){
 struct CSG** createTable() {
   struct CSG** csgTab = malloc(1009*sizeof(struct CSG));
   for (int i = 0; i < 1009; i++) {
-    csgTab[i] = newCSG("", 0, ""); //testing123
+    csgTab[i] = newCSG("", 0, "");
   }
   return csgTab;
 }
