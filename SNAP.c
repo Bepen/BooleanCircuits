1dc1b22ab01bb1792127a5c38a2861f9e46dd270
Bepen Neupane <bepen3@gmail.com>
2017-11-28 17:33:41 -0500

    🆑🅾️U🚹

--- SNAP.c
+++ SNAP.c
@@ -83,7 +83,7 @@ void printSNAPTable(struct SNAP** snapTable) {
         }
         else {
           //printf("Else Statement\n");
-          printf("Student ID: %d. Name: %s. Address: %s. Phone: %s. Key %d\n", snapTable[i]->studentID, snapTable[i]->name, snapTable[i]->address, snapTable[i]->phone, getSNAPHashKey(snapTable[i]));
+          printf("Student ID: %d  Name: %s  Address: %s  Phone: %s\n", snapTable[i]->studentID, snapTable[i]->name, snapTable[i]->address, snapTable[i]->phone);
           snapTable[i] = snapTable[i]->next;
         }
         if (snapTable[i] == NULL) {
@@ -96,7 +96,7 @@ void printSNAPTable(struct SNAP** snapTable) {
 }
 
 void lookupSNAP(struct SNAP** snapTable, int studentID, char* name, char* address, char* phone) {
-  struct SNAP* snapTemp = newSNAP(studentID, name, address, phone);
+  //struct SNAP* snapTemp = newSNAP(studentID, name, address, phone);
   struct SNAP** snapTableTemp = createSNAPTable();
   //Only Name is defined
   if (studentID == 0 && strcmp(address, "*") == 0 && strcmp(phone, "*") == 0) {
@@ -122,18 +122,24 @@ void lookupSNAP(struct SNAP** snapTable, int studentID, char* name, char* addres
   printSNAPTable(snapTable);
 }
 
-int main(int argc, char* argv[]) {
+void runSNAP() {
   struct SNAP** snapTable = createSNAPTable();
   struct SNAP** snapTable2 = createSNAPTable();
+  printf("\nRunning SNAP Relation***********************\n");
+  printf("Inserting 3 different tuples: \n");
+  printf("C. Brown with student ID 12345 from 12 Apple St. 555-1234\n");
+  printf("L.Van Pelt with student ID 67890 from 34 Pear Ave. 555-5678\n");
+  printf("P. Patty with student ID 22222 from 56 Grape Blvd. 555-9999\n");
   insertSNAPTuple(snapTable, 12345, "C. Brown", "12 Apple St.", "555-1234");
   insertSNAPTuple(snapTable, 67890, "L. Van Pelt", "34 Pear Ave.", "555-5678");
   insertSNAPTuple(snapTable, 22222, "P. Patty", "56 Grape Blvd.", "555-9999");
+  printf("Deleting P. Patty's information\n");
   deleteSNAPTuple(snapTable, 22222, "P. Patty", "56 Grape Blvd.", "555-9999");
+  //testing snapTable, remove before submission
   insertSNAPTuple(snapTable2, 12345, "C. Brown", "12 Apple St.", "555-1234");
-  insertSNAPTuple(snapTable2, 67890, "L. Van Pelt", "34 Pear Ave.", "555-5678");
-  insertSNAPTuple(snapTable2, 22222, "P. Patty", "56 Grape Blvd.", "555-9999");
-  deleteSNAPTuple(snapTable2, 22222, "P. Patty", "56 Grape Blvd.", "555-9999");
+  printf("Looking up the address and phone number for C.Brown using Student ID:\n");
+  lookupSNAP(snapTable2, 12345, "C. Brown", "*", "*");
+  printf("\nPrinting SNAP Table:\n");
   printSNAPTable(snapTable);
-  printf("Lookup***********\n");
-  lookupSNAP(snapTable2, 0, "C. Brown", "*", "*");
+  printf("Ending SNAP Relation***********************\n\n");
 }
--- main.c
+++ main.c
@@ -1,10 +1,12 @@
 #include "main.h"
 #include "CSG.c"
+#include "SNAP.c"
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char* argv[]) {
   runCSG();
+  runSNAP();
   return 0;
 }
